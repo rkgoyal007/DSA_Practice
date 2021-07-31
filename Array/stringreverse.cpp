d32a71a98b6fb593fe457073474a370284ba6e2f
RAHUL GOYAL <rkgoyal007@gmail.com>
2021-07-31 19:26:41 +0530

    added some more program files

---
+++ Array/stringreverse.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to reverse a string
+void reverse(string str)
+{
+   for (int i=str.length()-1; i>=0; i--){
+      cout << str[i];
+}
+ }
+// Driver code
+int main(void)
+{
+    string s = "GeeksforGeeks";
+    reverse(s);
+    return (0);
+}
